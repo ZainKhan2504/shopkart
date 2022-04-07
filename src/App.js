b79d2c4dba53aa3cf569e66f36d0641e9971f887
zaikh847 <zaikh847@student.liu.se>
2022-04-07 17:36:39 +0500

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div style={{ display: "flex", alignItems: "space-between" }}>
+      <span className="logo">SHOP KART</span>
+      <div>
+        <Link to="/" className="navLink">
+          Home
+        </Link>
+        <Link to="/cart" className="navLink">
+          Cart
+        </Link>
+        <span className="cartCount">Cart items: 0</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
